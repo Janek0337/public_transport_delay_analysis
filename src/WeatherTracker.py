0377f76213d5533f72aab21d85809b2aee5dd3da
Janek0337 <artyleriapolska@gmail.com>
2026-04-12 18:31:22 +0200

    zapisywanie warunków pogodowych

---
+++ src/WeatherTracker.py
@@ -0,0 +1,72 @@
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+from src.utils import oblicz_odleglosc
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+class StanPogody:
+    def __init__(self, lat, lon, temperatura, opad_deszczu, czy_dzien, opad_sniegu, poryw_wiatru):
+        self.lat = lat
+        self.lon = lon
+        self.temperatura = temperatura
+        self.opad_deszczu = opad_deszczu
+        self.czy_dzien = czy_dzien
+        self.opad_sniegu = opad_sniegu
+        self.poryw_wiatru = poryw_wiatru
+
+class WeatherTracker:
+    def __init__(self, punkty_pomiarowe: list[tuple[float, float]]):
+        self.dokladnosc = 1e7
+        self.url = "https://api.open-meteo.com/v1/forecast"
+        self.stacje_pomiarowe = [(int(x[0]*self.dokladnosc), int(x[1]*self.dokladnosc)) for x in punkty_pomiarowe]
+        self.cechy_pogody = ["temperature_2m", "rain", "showers", "is_day", "snowfall", "wind_gusts_10m"]
+        self.warunki = self.make_call()
+        self.ostatni_update = time.time()
+        self.update_co_sekund = 30 * 60
+        
+    def make_call(self) -> dict[tuple, StanPogody]:
+        logger.info(f"Pobieram informacje o pogodzie...")
+        cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+        retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+        openmeteo = openmeteo_requests.Client(session = retry_session)
+
+        # Make sure all required weather variables are listed here
+        # The order of variables in hourly or daily is important to assign them correctly below
+        params = {
+            "latitude": [x[0]/self.dokladnosc for x in self.stacje_pomiarowe],
+            "longitude": [x[1]/self.dokladnosc for x in self.stacje_pomiarowe],
+            "current": self.cechy_pogody,
+            "timezone": "Europe/Berlin",
+        }
+        responses = openmeteo.weather_api(self.url, params=params)
+
+        # Process first location. Add a for-loop for multiple locations or weather models
+        stany_pogody = dict()
+        for _, location in enumerate(responses):
+            lat = location.Latitude()
+            lon = location.Longitude()
+            # Process current data. The order of variables needs to be the same as requested.
+            current = location.Current()
+            temperature = current.Variables(0).Value()
+            rain = current.Variables(1).Value()
+            shower = current.Variables(2).Value()
+            is_day = current.Variables(3).Value()
+            snowfall = current.Variables(4).Value()
+            wind_gust = current.Variables(5).Value()
+
+            stany_pogody[(int(lat*self.dokladnosc), int(lon*self.dokladnosc))] = StanPogody(lat, lon, int(round(temperature)), round(rain + shower, 1), int(round(is_day)), round(snowfall, 1), int(round(wind_gust)))
+
+        return stany_pogody
+    
+    def pogoda_dla_punktu(self, lat, lon) -> StanPogody:
+        obecny_czas = time.time()
+        if obecny_czas - self.ostatni_update > self.update_co_sekund:
+            self.warunki = self.make_call()
+            self.ostatni_update = obecny_czas
+
+        najblizszy_punkt = min(self.warunki,
+                                key=lambda punkt: oblicz_odleglosc(lat, lon, punkt[0]/self.dokladnosc, punkt[1]/self.dokladnosc))
+        return self.warunki[najblizszy_punkt]
--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import src.kolektor_danych as kolektor_danych
 from src.TrackerZTM import TrackerZTM
 import time
 from pathlib import Path
+from src.WeatherTracker import WeatherTracker
 
 ROOT_DIR = Path(__file__).resolve().parent.parent
 DATA_DIR = ROOT_DIR / 'data'
@@ -28,13 +29,24 @@ def main():
         exit(1)
 
     kolektor_danych.stworz_baze_polozen_przystankow(API_KEY)
-    linie = ['523']
+    linie = ['523', '148']
     for linia in linie:
         kolektor_danych.stworz_trase_linii(API_KEY, linia)
         kolektor_danych.stworz_rozklad_linii(API_KEY, linia)
 
     tracker = TrackerZTM(linie)
+
+    punkt_wiatraczna = (52.245051314579435, 21.084365124034683)
+    punkt_politechnika = (52.245051314579435, 21.084365124034683)
+    punkt_metro_bemowo = (52.239277502745445, 20.913181966913832)
+    punkt_sggw = (52.158511701349056, 21.0477039225247)
+    punkt_dworzec_zachodni = (52.218013523486285, 20.96285349533098)
+    punkt_siekierki = (52.192605097067535, 21.04882025589122)
+    punkty_pogodowe = [punkt_wiatraczna, punkt_politechnika, punkt_metro_bemowo, punkt_sggw, punkt_dworzec_zachodni, punkt_siekierki]
+    pogoda = WeatherTracker(punkty_pogodowe)
+
     with open(f'{DATA_DIR}/out.csv', 'w') as f:
+        f.write("czas_str;linia;brygada;nazwa_trasy;lat;lon;metr;znak minuty:sekundy;opoznienie;temp;czy_dzien;opad_deszczu;opad_sniegu;poryw_wiatru\n")
         while True:
             logging.info("Pobieram dane o położeniu...")
             czas_start = time.time()
@@ -61,8 +73,15 @@ def main():
                     abs_opoznienie = abs(int(opoznienie))
                     minuty, sekundy = divmod(abs_opoznienie, 60)
 
+                    pogoda_tu = pogoda.pogoda_dla_punktu(lat, lon)
+                    temp = pogoda_tu.temperatura
+                    poryw_wiatru = pogoda_tu.poryw_wiatru
+                    opad_deszczu = pogoda_tu.opad_deszczu
+                    opad_sniegu = pogoda_tu.opad_sniegu
+                    czy_dzien = pogoda_tu.czy_dzien
+
                     logging.info(f"Pojazd {linia}/{brygada} | Trasa: {nazwa_trasy} | Metr: {metr:.2f}m | Status: {znak}{minuty}m {sekundy}s")
-                    linia_tekstu = f"{czas_str};{linia};{brygada};{nazwa_trasy};{metr:.2f};{znak}{minuty:02d}:{sekundy:02d};{opoznienie:.0f}\n"
+                    linia_tekstu = f"{czas_str};{linia};{brygada};{nazwa_trasy};{lat};{lon};{metr:.2f};{znak}{minuty:02d}:{sekundy:02d};{opoznienie:.0f};{temp};{czy_dzien};{opad_deszczu};{opad_sniegu};{poryw_wiatru}\n"
                     f.write(linia_tekstu)
 
             time.sleep(max(0, 16 - (time.time() - czas_start)))
